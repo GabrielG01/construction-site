2714b1f3fbaf3c090298503cd4ea42a79a81f17b
GabrielG01 <gabrielprovo01@gmail.com>
2019-11-21 14:48:57 -0700

    Removed initial react junk, input test h1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      // <Router>
+      //   <div>
+      //     <Switch>
+      //       <Route exact path="/" component={Home} />
+      //       <Route path="/art_posts" component={ArtPosts} />
+      //       <Route path="/create_post" component={CreatePost} />
+      //     </Switch>
+      //   </div>
+      // </Router>
+      <h1>Hello World!</h1>
+    );
+  }
 }
-
-export default App;
