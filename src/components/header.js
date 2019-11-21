eef60b7cab5b9b100d0924874eeea82c37e47c89
GabrielG01 <gabrielprovo01@gmail.com>
2019-11-21 16:16:01 -0700

    Fleshing out the styles for the header

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,27 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header-wrapper">
-        <div className="header_home">
-          <NavLink className="header_home" exact to="/">
-            Home
-          </NavLink>
-        </div>
+        <div className="header-content">
+          <div className="header-home-button">
+            <NavLink className="header-home" exact to="/">
+              Home
+            </NavLink>
+          </div>
 
-        <div className="header-nav-link-wrapper">
-          <NavLink className="header_services" exact to="/services">
-            Services
-          </NavLink>
-          <NavLink className="header_gallery" exact to="/gallery">
-            Gallery
-          </NavLink>
-          <NavLink className="header_contact" exact to="/contact">
-            Contact
-          </NavLink>
+          <div className="header-nav-link-wrapper">
+            <NavLink className="header-link" exact to="/">
+              <h3>Home</h3>
+            </NavLink>
+            <NavLink className="header-link" exact to="/services">
+              <h3>Services</h3>
+            </NavLink>
+            <NavLink className="header-link" exact to="/gallery">
+              <h3>Gallery</h3>
+            </NavLink>
+            <NavLink className="header-link" exact to="/contact">
+              <h3>Contact</h3>
+            </NavLink>
+          </div>
         </div>
       </div>
     );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import Header from "../components/header";
 
 export default class Contact extends Component {
+  constructor() {
+    super();
+
+    this.handleEmail = this.handleEmail.bind(this);
+  }
+
+  handleEmail() {
+    window.open("mailto:test@example.com");
+  }
+
   render() {
-    return <div className="contact-wrapper">Contact Page</div>;
+    return (
+      <div className="contact-wrapper">
+        <Header />
+        Contact Page
+        <button onClick={this.handleEmail}>Email Test</button>
+      </div>
+    );
   }
 }
