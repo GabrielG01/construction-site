243b60dcd763f506ca543071a2ea73ec25f0d897
GabrielG01 <gabrielprovo01@gmail.com>
2019-11-22 13:05:10 -0700

    Began on adding the content to the contact page, still rough doe

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,8 +17,24 @@ export default class Contact extends Component {
     return (
       <div className="contact-wrapper">
         <Header />
-        Contact Page
-        <button onClick={this.handleEmail}>Email Test</button>
+        <h1>Contact</h1>
+        <div className="contact-content-wrapper">
+          <div className="contact-top">
+            <div className="contact-address-wrapper">
+              <p>Peter Sweeney</p>
+              <p>777 Broadway Ave.</p>
+              <p>New York, NY 00000</p>
+            </div>
+            <div className="contact-email-wrapper">
+              <p>template.placeholder@gmail.com</p>
+            </div>
+          </div>
+          <div className="contact-bottom">
+            <div className="contact-phone-wrapper">
+              <p>212 555 0110</p>
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     );
