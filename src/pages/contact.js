d6212168cba0dc53edadee446cd7b7e568f9d6ae
GabrielG01 <gabrielprovo01@gmail.com>
2019-11-25 12:19:00 -0700

    Finished out the contact page XD

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import photo from "../images/stock3.jpg";
 
 export default class Contact extends Component {
   constructor() {
     super();
-
-    this.handleEmail = this.handleEmail.bind(this);
   }
 
   render() {
     return (
-      <div className="contact-wrapper">
+      <div className="contact">
         <Header />
-        <h1>Contact</h1>
-        <div className="contact-content-wrapper">
-          <div className="contact-top">
-            <div className="contact-address-wrapper">
-              <p>Peter Sweeney</p>
-              <p>777 Broadway Ave.</p>
-              <p>New York, NY 00000</p>
+        <div className="contact-wrapper">
+          <div className="contact-content-wrapper">
+            <div className="contact-header-wrapper">
+              <h1>Contact Us</h1>
             </div>
-            <div className="contact-email-wrapper">
-              <p>template.placeholder@gmail.com</p>
+
+            <div className="contact-top">
+              <div className="contact-address-wrapper">
+                <p>Peter Sweeney</p>
+                <p>777 Broadway Ave.</p>
+                <p>New York, NY 00000</p>
+              </div>
+              <div className="contact-email-wrapper">
+                <p>template.placeholder@gmail.com</p>
+              </div>
+            </div>
+            <div className="contact-bottom">
+              <div className="contact-phone-wrapper">
+                <p>212 555 0110</p>
+              </div>
             </div>
           </div>
-          <div className="contact-bottom">
-            <div className="contact-phone-wrapper">
-              <p>212 555 0110</p>
-            </div>
+          <div className="contact-image-wrapper">
+            <image src={photo} alt="contact-photo" />
           </div>
         </div>
         <Footer />
