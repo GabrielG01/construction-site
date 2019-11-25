fe712223c1239e2eae922cf341c71b93652ec5a4
GabrielG01 <gabrielprovo01@gmail.com>
2019-11-25 12:40:08 -0700

    Finished adding in the squares to the gallery page, also imported a bunch of stock images for said page

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import image_one from "../images/stock4.jpg";
+import image_two from "../images/stock5.jpg";
+import image_three from "../images/stock6.jpg";
+import image_four from "../images/stock7.jpg";
 
 export default class Gallery extends Component {
   render() {
     return (
       <div className="gallery-wrapper">
         <Header />
-        Gallery Page
+        <div className="gallery-header-wrapper">
+          <h1>Past Projects</h1>
+          <div className="gallery-squares-wrapper">
+            <div className="gallery-squares">
+              <div className="gallery-square">
+                <div className="gallery-img-wrapper">
+                  <img src={image_one} alt="gallery-image" />
+                </div>
+                <div className="gallery-square-text-wrapper">
+                  <p>Place holder text here :)</p>
+                </div>
+              </div>
+              <div className="gallery-square">
+                <div className="gallery-square-text-wrapper">
+                  <p>Place holder text here :)</p>
+                </div>
+                <div className="gallery-img-wrapper">
+                  <img src={image_two} alt="gallery-image" />
+                </div>
+              </div>
+              <div className="gallery-square">
+                <div className="gallery-img-wrapper">
+                  <img src={image_three} alt="gallery-image" />
+                </div>
+                <div className="gallery-square-text-wrapper">
+                  <p>Place holder text here :)</p>
+                </div>
+              </div>
+              <div className="gallery-square">
+                <div className="gallery-square-text-wrapper">
+                  <p>Placeholder text here :)</p>
+                </div>
+                <div className="gallery-img-wrapper">
+                  <img src={image_four} alt="gallery-image" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     );
